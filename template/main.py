01319cff5e688c21f6a9f48cd2d487d643f37e62
Jhe-Yun <tizan0826@gmail.com>
2021-04-18 12:51:43 +0800

    fix: template/

--- template/main.py
+++ template/main.py
@@ -4,9 +4,9 @@ def config():
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--consumption", default="training_data.csv", help="input the consumption data path")
-    parser.add_argument("--generation", default="testing_data.csv", help="input the generation data path")
-    parser.add_argument("--bidresult", default="output.csv", help="input the bids result path")
+    parser.add_argument("--consumption", default="./sample_data/consumption.csv", help="input the consumption data path")
+    parser.add_argument("--generation", default="./sample_data/generation.csv", help="input the generation data path")
+    parser.add_argument("--bidresult", default="./sample_data/bidresult.csv", help="input the bids result path")
     parser.add_argument("--output", default="output.csv", help="output the bids path")
 
     return parser.parse_args()
@@ -26,4 +26,4 @@ if __name__ == "__main__":
 
     data = [["2018-01-01 00:00:00", "buy", 2.5, 3],
             ["2018-01-01 01:00:00", "sell", 3, 5]]
-    output(args.consumption, args.output, data)
+    output(args.output, data)
